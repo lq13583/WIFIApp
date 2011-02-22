9149e1a87d9768fa19e533538a51c2f23786f33d
john <john@4b106dcd-2108-452d-b8fa-386b945ea3c6>
2011-02-22 18:38:54 +0000

    Update today's changes.

--- trunk/src/com/sunnybrook/SettingsActivity.java
+++ trunk/src/com/sunnybrook/SettingsActivity.java
@@ -2,6 +2,8 @@ package com.sunnybrook;
 
 import android.app.Activity;
 import android.os.Bundle;
+//import android.text.method.ScrollingMovementMethod;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class SettingsActivity extends Activity{
@@ -16,7 +18,28 @@ public class SettingsActivity extends Activity{
     }
     
     private void showSettings(sysconfig mSysconfig) {
-    	EditText mEditText= (EditText) findViewById(R.id.jdbc_url);
+    	
+    	EditText mEditText = (EditText) findViewById(R.id.labor_code);
+    	mEditText.setText(mSysconfig.getLabor_code());
+    	mEditText = (EditText) findViewById(R.id.labor_name);
+    	mEditText.setText(mSysconfig.getLabor_name());
+    	mEditText = (EditText) findViewById(R.id.jdbc_url);
     	mEditText.setText(mSysconfig.getJdbc_url());
+//    	mEditText.setMovementMethod(new ScrollingMovementMethod());
+
+    	mEditText = (EditText) findViewById(R.id.update_int);
+    	mEditText.setText(Integer.toString(mSysconfig.getUpdate_int()));
+    	mEditText = (EditText) findViewById(R.id.update_int_max);
+    	mEditText.setText(Integer.toString(mSysconfig.getUpdate_int_max()));
+    	mEditText = (EditText) findViewById(R.id.font_size);
+    	mEditText.setText(Integer.toString(mSysconfig.getFont_size()));
+    	mEditText = (EditText) findViewById(R.id.desc_font_size);
+    	mEditText.setText(Integer.toString(mSysconfig.getDesc_font_size()));
+
+    	CheckBox mCheckBox = (CheckBox) findViewById(R.id.is_super);
+    	mCheckBox.setChecked(mSysconfig.is_super());
+    	mCheckBox = (CheckBox) findViewById(R.id.update_key);
+    	mCheckBox.setChecked(mSysconfig.isUpdate_key());
+    	
     }
 }
