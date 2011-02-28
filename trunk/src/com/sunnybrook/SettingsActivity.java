7f41ffdf4d923447016a3fe5c0572ca843876bfc
john <john@4b106dcd-2108-452d-b8fa-386b945ea3c6>
2011-02-28 04:33:01 +0000

    Today's changes.

--- trunk/src/com/sunnybrook/SettingsActivity.java
+++ trunk/src/com/sunnybrook/SettingsActivity.java
@@ -2,10 +2,8 @@ package com.sunnybrook;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-//import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -21,13 +19,34 @@ public class SettingsActivity extends Activity{
     	mLocalDB = new localDB(this);
     	mSysconfig = new sysconfig(mLocalDB);
     	showSettings(mSysconfig);
-    	setButtonEvents();
     }
-    
-    private void setButtonEvents() {
+
+    private void showSettings(sysconfig mSysconfig) {
+    	
+    	EditText mEditText = (EditText) findViewById(R.id.labor_code);
+    	mEditText.setText(mSysconfig.getLabor_code());
+    	mEditText = (EditText) findViewById(R.id.labor_name);
+    	mEditText.setText(mSysconfig.getLabor_name());
+    	mEditText = (EditText) findViewById(R.id.jdbc_url);
+    	mEditText.setText(mSysconfig.getJdbc_url());
+
+    	mEditText = (EditText) findViewById(R.id.update_int);
+    	mEditText.setText(Integer.toString(mSysconfig.getUpdate_int()));
+    	mEditText = (EditText) findViewById(R.id.update_int_max);
+    	mEditText.setText(Integer.toString(mSysconfig.getUpdate_int_max()));
+    	mEditText = (EditText) findViewById(R.id.font_size);
+    	mEditText.setText(Integer.toString(mSysconfig.getFont_size()));
+    	mEditText = (EditText) findViewById(R.id.desc_font_size);
+    	mEditText.setText(Integer.toString(mSysconfig.getDesc_font_size()));
+
+    	CheckBox mCheckBox = (CheckBox) findViewById(R.id.is_super);
+    	mCheckBox.setChecked(mSysconfig.is_super());
+    	mCheckBox = (CheckBox) findViewById(R.id.update_key);
+    	mCheckBox.setChecked(mSysconfig.isUpdate_key());
+    	
     	Button btn = (Button) findViewById(R.id.btnSave);
     	btn.setOnClickListener(btn_save_onclick_listener);
-    }
+    }    
     
     private OnClickListener btn_save_onclick_listener = new OnClickListener() {
     	public void onClick(View v) {
@@ -55,28 +74,5 @@ public class SettingsActivity extends Activity{
 		.show();
     }
     
-    private void showSettings(sysconfig mSysconfig) {
-    	
-    	EditText mEditText = (EditText) findViewById(R.id.labor_code);
-    	mEditText.setText(mSysconfig.getLabor_code());
-    	mEditText = (EditText) findViewById(R.id.labor_name);
-    	mEditText.setText(mSysconfig.getLabor_name());
-    	mEditText = (EditText) findViewById(R.id.jdbc_url);
-    	mEditText.setText(mSysconfig.getJdbc_url());
 
-    	mEditText = (EditText) findViewById(R.id.update_int);
-    	mEditText.setText(Integer.toString(mSysconfig.getUpdate_int()));
-    	mEditText = (EditText) findViewById(R.id.update_int_max);
-    	mEditText.setText(Integer.toString(mSysconfig.getUpdate_int_max()));
-    	mEditText = (EditText) findViewById(R.id.font_size);
-    	mEditText.setText(Integer.toString(mSysconfig.getFont_size()));
-    	mEditText = (EditText) findViewById(R.id.desc_font_size);
-    	mEditText.setText(Integer.toString(mSysconfig.getDesc_font_size()));
-
-    	CheckBox mCheckBox = (CheckBox) findViewById(R.id.is_super);
-    	mCheckBox.setChecked(mSysconfig.is_super());
-    	mCheckBox = (CheckBox) findViewById(R.id.update_key);
-    	mCheckBox.setChecked(mSysconfig.isUpdate_key());
-    	
-    }
 }
