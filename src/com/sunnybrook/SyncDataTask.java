5c6b10f6ee7295df4a847fd76afe74b12be46aea
john <john@4b106dcd-2108-452d-b8fa-386b945ea3c6>
2011-03-02 20:05:39 +0000

    Update today's changes.

--- src/com/sunnybrook/SyncDataTask.java
+++ src/com/sunnybrook/SyncDataTask.java
@@ -1,18 +1,19 @@
 package com.sunnybrook;
 
+import java.util.List;
 import java.util.TimerTask;
 
 public class SyncDataTask extends TimerTask {
-	private int mCounts = 0; 
-	private int mPeriod = 0;
-	private int maxPeriod = WIFIApp.myConfig.getUpdate_int_max()/WIFIApp.myConfig.getUpdate_int();
-	private boolean is_running = false;
-	private localDB myLocalDB = WIFIApp.localdb;
-	private String myJDBC_URL = WIFIApp.myConfig.getJdbc_url();
-	private remoteDB myRemoteDB;
+	private static int mCounts = 0; 
+	private static int mPeriod = 0;
+	private static boolean is_running = false;
+	private sysconfig myConfig = WIFIApp.myConfig;
+	private int maxPeriod = myConfig.getUpdate_int_max()/WIFIApp.myConfig.getUpdate_int();
 
 	@Override
 	public void run() {
+		localDB myLocalDB = WIFIApp.localdb;
+		remoteDB myRemoteDB;
 
 //Task is delayed until mCounts count down to 0		
 		if (mCounts>0){
@@ -26,7 +27,8 @@ public class SyncDataTask extends TimerTask {
 //Set the task running flag
 		is_running=true;
 
-		myRemoteDB = new remoteDB(myJDBC_URL);
+//Open remote DB connection
+		myRemoteDB = new remoteDB(myConfig.getJdbc_url());
 		
 		if (!myRemoteDB.isConnected()) {
 //Increase the waiting period if no network connection.		
@@ -43,10 +45,10 @@ public class SyncDataTask extends TimerTask {
 					
 				}
 			}
-				
 		}
 
-		//Clear the task running flag
+
+//Clear the task running flag
 		is_running = false;
 		return;
 	}
@@ -55,7 +57,31 @@ public class SyncDataTask extends TimerTask {
 		return true;
 	}
 	
+	private boolean pullOwnOrders(localDB localdb, remoteDB remotedb) {
+		List<ownorder> mList = remotedb.getOwnOrders(myConfig.getLabor_code());
+		return true;
+	}
+	
+	private boolean pullSuperOrders(localDB localdb, remoteDB remotedb) {
+		return true;
+	}
+	
+	private boolean pullCraftOrders(localDB localdb, remoteDB remotedb) {
+		return true;
+	}
+	
+	private boolean pullKeys(localDB localdb, remoteDB remotedb) {
+		return true;
+	}
+	
 	private boolean pullData(localDB localdb, remoteDB remotedb) {
+		if (!pullOwnOrders(localdb,remotedb)) return false;
+		if (myConfig.is_super())
+			if(!pullSuperOrders(localdb, remotedb)) return false;
+		if (!myConfig.getCraft_list().isEmpty())
+			if(!pullCraftOrders(localdb, remotedb)) return false;
+		if(myConfig.isUpdate_key())
+			if(!pullKeys(localdb,remotedb)) return false;
 		return true;
 	}
 }
--- src/com/sunnybrook/remoteDB.java
+++ src/com/sunnybrook/remoteDB.java
@@ -1,6 +1,8 @@
 package com.sunnybrook;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class remoteDB {
 	private String connStr;
@@ -39,4 +41,96 @@ public class remoteDB {
 		}
 	}
 	
+	public workorder getWorkOrder(String wonum) {
+		workorder mWorkOrder = null;
+		PreparedStatement mPs = null;
+		ResultSet mRs = null;
+		String sql = "select wo.wonum,wo.status,wo.statusdate,wo.description,wo.location,"
+				   + "wo.changeby,wo.changedate,wo.wopriority,wo.wo2,wo.wo3,des.ldtext comments,"
+				   + "wo.reportedby,wo.reportdate,wo.phone,loc.description locdesc,wo.empcomments remp," 
+				   + "khname,ktitle,kdept,kcamp,kcost,kcod1,kcodr1,kcod2,kcodr2,kcod3,kcodr3,kcod4,kcodr4," 
+                   + "kcor1,kcorr1,kcor2,kcorr2,kcor3,kcorr3,kcor4,kcorr4,kcom,kq1,kq2,kq3,kq4" 
+				   + " from workorder wo"
+				   + " left join longdescription des on des.ldkey = wo.ldkey"
+				   + " left join locations loc on loc.location = wo.location and loc.siteid = wo.siteid"
+				   + " where wo.wonum = ?";
+		try {
+			mPs = conn.prepareStatement(sql);
+			mPs.setString(1,wonum);
+			mRs = mPs.executeQuery();
+			if(mRs.next()) {
+				mWorkOrder = new workorder(mRs.getString("wonum"));
+				mWorkOrder.setStatus(mRs.getString("status"));
+				mWorkOrder.setStatusdate(mRs.getDate("statusdate"));
+				mWorkOrder.setDescription(mRs.getString("description"));
+				mWorkOrder.setLocation(mRs.getString("location"));
+				mWorkOrder.setChangedby(mRs.getString("changeby"));
+				mWorkOrder.setChangedate(mRs.getDate("changedate"));
+				mWorkOrder.setWopriority(mRs.getInt("wopriority"));
+				mWorkOrder.setWo2(mRs.getString("wo2"));
+				mWorkOrder.setWo3(mRs.getString("wo3"));
+				mWorkOrder.setComments(mRs.getString("comments"));
+				mWorkOrder.setReportedby(mRs.getString("reportedby"));
+				mWorkOrder.setReportdate(mRs.getDate("reportdate"));
+				mWorkOrder.setPhone(mRs.getString("phone"));
+				mWorkOrder.setLocationdesc(mRs.getString("locdesc"));
+				mWorkOrder.setEmpcomments(mRs.getString("remp"));
+				mWorkOrder.setKhname(mRs.getString("khname"));
+				mWorkOrder.setKtitle(mRs.getString("ktitle"));
+				mWorkOrder.setKdept(mRs.getString("kdept"));
+				mWorkOrder.setKcamp(mRs.getString("kcamp"));
+				mWorkOrder.setKcost(mRs.getString("kcost"));
+				mWorkOrder.setKcod1(mRs.getString("kcod1"));
+				mWorkOrder.setKcodr1(mRs.getString("kcodr1"));
+				mWorkOrder.setKcod2(mRs.getString("kcod2"));
+				mWorkOrder.setKcodr2(mRs.getString("kcodr2"));
+				mWorkOrder.setKcod3(mRs.getString("kcod3"));
+				mWorkOrder.setKcodr3(mRs.getString("kcodr3"));
+				mWorkOrder.setKcod4(mRs.getString("kcod4"));
+				mWorkOrder.setKcodr4(mRs.getString("kcodr4"));
+				mWorkOrder.setKcor1(mRs.getString("kcor1"));
+				mWorkOrder.setKcorr1(mRs.getString("kcorr1"));
+				mWorkOrder.setKcor2(mRs.getString("kcor2"));
+				mWorkOrder.setKcorr2(mRs.getString("kcorr2"));
+				mWorkOrder.setKcor3(mRs.getString("kcor3"));
+				mWorkOrder.setKcorr3(mRs.getString("kcorr3"));
+				mWorkOrder.setKcor4(mRs.getString("kcor4"));
+				mWorkOrder.setKcorr4(mRs.getString("kcorr4"));
+				mWorkOrder.setKcom(mRs.getString("kcom"));
+				mWorkOrder.setKq1(mRs.getString("kq1"));
+				mWorkOrder.setKq2(mRs.getString("kq2"));
+				mWorkOrder.setKq3(mRs.getString("kq3"));
+				mWorkOrder.setKq4(mRs.getString("kq4"));
+			}
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return mWorkOrder;
+	}
+	
+	public List<ownorder> getOwnOrders(String labor_code) {
+		List<ownorder> orderList = new ArrayList<ownorder>();
+		PreparedStatement mPs = null;
+		ResultSet mRs = null;
+		String sql = "select wo.wonum from workorder wo"
+				   + " join assignment ass on ass.wonum = wo.wonum and ass.craft= ? "
+				   + " where wo.siteid = 'MAXSITE' and wo.status in ('INPRG','WMATL') and (wo.worktype like 'C-%' or wo.worktype is null)"
+				   + " order by wo.wonum";
+		try {
+			mPs = conn.prepareStatement(sql);
+			mPs.setString(1,labor_code);
+			mRs = mPs.executeQuery();
+			while (mRs.next()) {
+				ownorder mOwnorder = (ownorder) getWorkOrder(mRs.getString("wonum"));
+				mOwnorder.setReadStatus("UR");
+				orderList.add(mOwnorder);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return orderList;
+	}
 }
