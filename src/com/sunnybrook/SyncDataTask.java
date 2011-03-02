723daf6763090eb7ca39092041135a554234fdc4
john <john@4b106dcd-2108-452d-b8fa-386b945ea3c6>
2011-03-02 02:46:00 +0000

    Update Today's changes.

--- src/com/sunnybrook/SyncDataTask.java
+++ src/com/sunnybrook/SyncDataTask.java
@@ -14,32 +14,48 @@ public class SyncDataTask extends TimerTask {
 	@Override
 	public void run() {
 
-//Task is still running
-		if(is_running) return; 
-
 //Task is delayed until mCounts count down to 0		
 		if (mCounts>0){
 			mCounts--;
 			return;
 		}
+
+//Task is still running
+		if(is_running) return; 
+
 //Set the task running flag
 		is_running=true;
 
 		myRemoteDB = new remoteDB(myJDBC_URL);
 		
-		if (myRemoteDB.isConnected()) {
-			mPeriod = 0;
-		}
-		else {
+		if (!myRemoteDB.isConnected()) {
 //Increase the waiting period if no network connection.		
 			if (mPeriod < maxPeriod) {
 				mPeriod++;
 			}		
 			mCounts += mPeriod;
-
-//Clear the task running flag
-			is_running = false;
 		}
+		else {
+//Reset waiting period to 0;			
+			mPeriod = 0;
+			if(pushData(myLocalDB,myRemoteDB)){
+				if(pullData(myLocalDB,myRemoteDB)) {
+					
+				}
+			}
+				
+		}
+
+		//Clear the task running flag
+		is_running = false;
+		return;
 	}
 
+	private boolean pushData(localDB localdb, remoteDB remotedb){
+		return true;
+	}
+	
+	private boolean pullData(localDB localdb, remoteDB remotedb) {
+		return true;
+	}
 }
